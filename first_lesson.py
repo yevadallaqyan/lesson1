75966f0e421b8affa19d1d615bdc673ae8a98c74
yevadallaqyan <eva.dallaqyan@edu.ysu.am>
2025-09-14 20:27:22 +0400

    Update first_lesson.py

--- first_lesson.py
+++ first_lesson.py
@@ -1,2 +1,12 @@
-print("hello world")
-print("my name is Yeva")
+#Parz tveri xndiry
+
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+
+for num in range(a,b+1):
+    if num>1:
+        for i in range(2,num):
+            if num % i == 0:
+                break
+        else:
+         print(num)
